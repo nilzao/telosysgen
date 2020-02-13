372e58b8fe138a30e5db7079dc4f730eb2cc6f14
Nilzao <288571+nilzao@users.noreply.github.com>
2020-02-13 12:55:14 -0400

    https://spring.io/guides/gs/accessing-data-rest/ example

---
+++ src/main/java/org/telosysgen/AccessingDataRestApplication.java
@@ -0,0 +1,13 @@
+package org.telosysgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// from https://spring.io/guides/gs/accessing-data-rest/
+@SpringBootApplication
+public class AccessingDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingDataRestApplication.class, args);
+	}
+}
