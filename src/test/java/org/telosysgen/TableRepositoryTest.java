db8d7b01a9fe61ddfbf133f0b006535e1d53499e
Nilzao <288571+nilzao@users.noreply.github.com>
2020-02-14 20:28:14 -0400

    jpa testing

---
+++ src/test/java/org/telosysgen/TableRepositoryTest.java
@@ -0,0 +1,34 @@
+package org.telosysgen;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class TableRepositoryTest {
+
+	@Autowired
+	private TestEntityManager entityManager;
+
+	@Test
+	public void saveTable() {
+		TbDatabaseJpaRecord tbDatabaseJpaRecord = new TbDatabaseJpaRecord();
+		tbDatabaseJpaRecord.setDatabaseName("arroy");
+		TbTableJpaRecord tbTableJpaRecord = new TbTableJpaRecord();
+		tbTableJpaRecord.setName("table test");
+		tbTableJpaRecord.setDatabase(tbDatabaseJpaRecord);
+		entityManager.persistAndFlush(tbDatabaseJpaRecord);
+		entityManager.persistAndFlush(tbTableJpaRecord);
+		TbTableJpaRecord find = entityManager.find(TbTableJpaRecord.class, 1l);
+		assertEquals(find.getName(), "table test");
+		assertEquals(find.getDatabase().getDatabaseName(), "arroy");
+	}
+}
