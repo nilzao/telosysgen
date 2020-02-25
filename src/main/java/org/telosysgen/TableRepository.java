3d2f7dce8318f18d94c5b4ce9154785a58031a24
Nilzao <288571+nilzao@users.noreply.github.com>
2020-02-25 11:29:56 -0400

    find table by name

--- src/main/java/org/telosysgen/TableRepository.java
+++ src/main/java/org/telosysgen/TableRepository.java
@@ -1,8 +1,16 @@
 package org.telosysgen;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "table", path = "table")
 public interface TableRepository extends PagingAndSortingRepository<TbTableJpaRecord, Long> {
+
+	@Query("SELECT obj from TbTableJpaRecord obj where obj.name like :name%")
+	List<TbTableJpaRecord> findByName(@Param("name") String name, Pageable pageable);
 }
--- tsg-extjs/app/view/grid/tbar/TableCombo.js
+++ tsg-extjs/app/view/grid/tbar/TableCombo.js
@@ -5,17 +5,25 @@ Ext.define('TelosysGen.view.grid.tbar.TableCombo', {
 	queryMode : 'remote',
 	displayField : 'name',
 	valueField : 'id',
+	queryParam : 'name',
 	store : {
 		type : 'table'
 	},
 	listeners : {
-		change : function(thisObj, newValue, oldValue, eOpts) {
-			var gridStore = thisObj.findParentByType("grid").getStore();
+		select : function(combo, record, eOpts) {
+			var gridStore = combo.findParentByType("grid").getStore();
 			gridStore.getProxy().setUrl('http://localhost:8080/column/search/findByTable_IdTable');
 			gridStore.getProxy().setExtraParams({
-				tableId : newValue
+				tableId : record.getData().id
 			});
 			gridStore.load();
+		},
+		beforequery : function(queryPlan, eOpts) {
+			if (queryPlan.query == "") {
+				queryPlan.combo.getStore().getProxy().setUrl('http://localhost:8080/table');
+			} else {
+				queryPlan.combo.getStore().getProxy().setUrl('http://localhost:8080/table/search/findByName');
+			}
 		}
 	}
 });
